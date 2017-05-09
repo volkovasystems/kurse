b0d2b0880753230907c5d548047d26500bc49a98
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-09 12:02:05 +0800

    Remove use strict on test.

--- kurse-test.js
+++ kurse-test.js
@@ -1,9 +1,6 @@
-"use strict";
-
 const assert = require( "assert" );
 const kurse = require( "./kurse.js" );
 
 assert.ok( kurse( { "name": "simple" } )[ ID ] );
-console.log( kurse( { "name": "simple" } )[ ID ] )
 
 console.log( "ok" );
