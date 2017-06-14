e9cf1535dd99ef77e3478c2cdc8018ee18b0b31c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-14 12:11:43 +0800

    Updated server/client test.

--- kurse-test.js
+++ kurse-test.js
@@ -2,8 +2,6 @@ const assert = require( "assert" );
 const kurse = require( "./kurse.js" );
 const mrkd = require( "mrkd" );
 
-assert.ok( kurse( { "name": "simple" } ) );
-
 assert.equal( mrkd( Symbol( "id" ), kurse( { "name": "simple" } ) ), true, "should be equal" );
 
 console.log( "ok" );
